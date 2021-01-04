d33a8effc459ce5a986fffe7e9d884c4cbab7bad
Roland Puntaier <roland.puntaier@gmail.com>
2021-01-04 12:36:12 +0100

    lite

---
+++ course_lite/2catsdogs.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python
+
+# https://github.com/tensorflow/examples/blob/master/courses/udacity_intro_to_tensorflow_lite/tflite_c02_transfer_learning.ipynb
+
+# Transfer Learning with TensorFlow Hub for TFLite
+## Setup 
+
+import os
+import pathlib
+import numpy as np
+
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+import tensorflow_hub as hub
+
+print("Version: ", tf.__version__)
+print("Eager mode: ", tf.executing_eagerly())
+print("Hub version: ", hub.__version__)
+print("GPU is", "available" if tf.test.is_gpu_available() else "NOT AVAILABLE")
+
+## Select the Hub/TF2 module to use
+#Hub modules for TF 1.x won't work here, please use one of the selections provided.
+
+module_selection = ("mobilenet_v2", 224, 1280) #@param ["(\"mobilenet_v2\", 224, 1280)", "(\"inception_v3\", 299, 2048)"] {type:"raw", allow-input: true}
+handle_base, pixels, FV_SIZE = module_selection
+MODULE_HANDLE ="https://tfhub.dev/google/tf2-preview/{}/feature_vector/4".format(handle_base)
+IMAGE_SIZE = (pixels, pixels)
+print("Using {} with input size {} and output dimension {}".format(
+  MODULE_HANDLE, IMAGE_SIZE, FV_SIZE))
+
+## Data preprocessing
+# Use [TensorFlow Datasets](http://tensorflow.org/datasets) to load the cats and
+# dogs dataset.  This `tfds` package is the easiest way to load pre-defined data.
+# If you have your own data, and are interested in importing using it with
+# TensorFlow see [loading image data](../load_data/images.ipynb)
+
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+# The `tfds.load` method downloads and caches the data, and returns a
+# `tf.data.Dataset` object. These objects provide powerful, efficient methods for
+# manipulating data and piping it into your model.
+# Since `"cats_vs_dog"` doesn't define standard splits, use the subsplit feature
+# to divide it into (train, validation, test) with 80%, 10%, 10% of the data
+# respectively.
+
+(train_examples, validation_examples, test_examples), info = tfds.load(
+    'cats_vs_dogs',
+    split=['train[80%:]', 'train[80%:90%]', 'train[90%:]'],
+    with_info=True, 
+    as_supervised=True, 
+)
+num_examples = info.splits['train'].num_examples
+num_classes = info.features['label'].num_classes
+
+### Format the Data
+# Use the `tf.image` module to format the images for the task.
+# Resize the images to a fixes input size, and rescale the input channels
+
+def format_image(image, label):
+  image = tf.image.resize(image, IMAGE_SIZE) / 255.0
+  return  image, label
+
+#Now shuffle and batch the data
+
+BATCH_SIZE = 32 #@param {type:"integer"}
+
+train_batches = train_examples.shuffle(num_examples // 4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+validation_batches = validation_examples.map(format_image).batch(BATCH_SIZE).prefetch(1)
+test_batches = test_examples.map(format_image).batch(1)
+
+#Inspect a batch
+for image_batch, label_batch in train_batches.take(1):
+  pass
+#image_batch.shape
+
+## Defining the model
+# All it takes is to put a linear classifier on top of the
+# `feature_extractor_layer` with the Hub module.
+# For speed, we start out with a non-trainable `feature_extractor_layer`, but you
+# can also enable fine-tuning for greater accuracy.
+
+do_fine_tuning = False #@param {type:"boolean"}
+
+#Load TFHub Module
+
+feature_extractor = hub.KerasLayer(MODULE_HANDLE,
+                                   input_shape=IMAGE_SIZE + (3,), 
+                                   output_shape=[FV_SIZE],
+                                   trainable=do_fine_tuning)
+print("Building model with", MODULE_HANDLE)
+model = tf.keras.Sequential([
+    feature_extractor,
+    tf.keras.layers.Dense(num_classes)
+])
+model.summary()
+
+#@title (Optional) Unfreeze some layers
+NUM_LAYERS = 7 #@param {type:"slider", min:1, max:50, step:1}
+if do_fine_tuning:
+  feature_extractor.trainable = True
+  for layer in model.layers[-NUM_LAYERS:]:
+    layer.trainable = True
+else:
+  feature_extractor.trainable = False
+
+## Training the model
+
+if do_fine_tuning:
+  model.compile(
+    optimizer=tf.keras.optimizers.SGD(lr=0.002, momentum=0.9), 
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+else:
+  model.compile(
+    optimizer='adam', 
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+
+EPOCHS = 5
+hist = model.fit(train_batches,
+                    epochs=EPOCHS,
+                    validation_data=validation_batches,
+                    verbose=0)
+
+## Export the model
+CATS_VS_DOGS_SAVED_MODEL = "catsdogsdata/1"
+
+#Export the SavedModel
+tf.saved_model.save(model, CATS_VS_DOGS_SAVED_MODEL)
+
+#! saved_model_cli show --dir catsdogsdata --tag_set serve --signature_def serving_default
+
+loaded = tf.saved_model.load(CATS_VS_DOGS_SAVED_MODEL)
+
+print(list(loaded.signatures.keys()))
+infer = loaded.signatures["serving_default"]
+print(infer.structured_input_signature)
+print(infer.structured_outputs)
+
+## Convert using TFLite's Converter
+
+#Load the TFLiteConverter with the SavedModel
+
+converter = tf.lite.TFLiteConverter.from_saved_model(CATS_VS_DOGS_SAVED_MODEL)
+
+### Post-training quantization
+# The simplest form of post-training quantization quantizes weights from floating
+# point to 8-bits of precision. This technique is enabled as an option in the
+# TensorFlow Lite converter. At inference, weights are converted from 8-bits of
+# precision to floating point and computed using floating-point kernels. This
+# conversion is done once and cached to reduce latency.
+# To further improve latency, hybrid operators dynamically quantize activations
+# to 8-bits and perform computations with 8-bit weights and activations. This
+# optimization provides latencies close to fully fixed-point inference. However,
+# the outputs are still stored using floating point, so that the speedup with
+# hybrid ops is less than a full fixed-point computation.
+
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+### Post-training integer quantization
+# We can get further latency improvements, reductions in peak memory usage, and
+# access to integer only hardware accelerators by making sure all model math is
+# quantized. To do this, we need to measure the dynamic range of activations and
+# inputs with a representative data set. You can simply create an input data
+# generator and provide it to our converter.
+
+def representative_data_gen():
+  for input_value, _ in test_batches.take(100):
+    yield [input_value]
+
+converter.representative_dataset = representative_data_gen
+
+# The resulting model will be fully quantized but still take float input and
+# output for convenience.
+# Ops that do not have quantized implementations will automatically be left in
+# floating point. This allows conversion to occur smoothly but may restrict
+# deployment to accelerators that support float. 
+
+### Full integer quantization
+# To require the converter to only output integer operations, one can specify:
+
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+
+### Finally convert the model
+
+mkdir = lambda p: p.parent.mkdir(parents=True,exist_ok=True) or p
+tflite_model = converter.convert()
+tflite_model_file = pathlib.Path('catsdogsdata/catsdogs.tflite')
+mkdir(tflite_model_file).write_bytes(tflite_model)
+##Test the TFLite model using the Python Interpreter
+
+# Load TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path=str(tflite_model_file))
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]["index"]
+output_index = interpreter.get_output_details()[0]["index"]
+
+from tqdm import tqdm
+
+predictions = []
+
+test_labels, test_imgs = [], []
+for img, label in tqdm(test_batches.take(10)):
+  interpreter.set_tensor(input_index, img)
+  interpreter.invoke()
+  predictions.append(interpreter.get_tensor(output_index))
+  test_labels.append(label.numpy()[0])
+  test_imgs.append(img)
+
+#@title Utility functions for plotting
+# Utilities for plotting
+
+class_names = ['cat', 'dog']
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  img = np.squeeze(img)
+  plt.imshow(img, cmap=plt.cm.binary)
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'green'
+  else:
+    color = 'red'
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+
+# NOTE: Colab runs on server CPUs. At the time of writing this, TensorFlow Lite
+# doesn't have super optimized server CPU kernels. For this reason post-training
+# full-integer quantized models  may be slower here than the other kinds of
+# optimized models. But for mobile CPUs, considerable speedup can be observed.
+
+#@title Visualize the outputs { run: "auto" }
+index = 0 #@param {type:"slider", min:0, max:9, step:1}
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(index, predictions, test_labels, test_imgs)
+plt.show()
+
+# # Download the model.
+# # **NOTE: You might have to run to the cell below twice**
+# 
+# labels = ['cat', 'dog']
+# with open('catsdogsdata/catsdogslabels.txt', 'w') as f:
+#   f.write('\n'.join(labels))
+# try:
+#   from google.colab import files
+#   files.download('catsdogsdata/catsdogs.tflite')
+#   files.download('catsdogsdata/catsdogslabels.txt')
+# except:
+#   pass
+# 
+# # Prepare the test images for download (Optional)
+# # This part involves downloading additional test images for the Mobile Apps only
+# # in case you need to try out more samples
+# 
+# #!mkdir -p test_images
+# 
+# from PIL import Image
+# 
+# for index, (image, label) in enumerate(test_batches.take(50)):
+#   image = tf.cast(image * 255.0, tf.uint8)
+#   image = tf.squeeze(image).numpy()
+#   pil_image = Image.fromarray(image)
+#   pil_image.save('test_images/{}_{}.jpg'.format(class_names[label[0]], index))
+# 
+# # !ls test_images
+# 
+# # !zip -qq cats_vs_dogs_test_images.zip -r test_images/
+# 
+# try:
+#   files.download('cats_vs_dogs_test_images.zip')
+# except:
+#   pass
---
+++ course_lite/34fashion.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python
+# https://github.com/tensorflow/examples/blob/master/courses/udacity_intro_to_tensorflow_lite/tflite_c03_exercise_convert_model_to_tflite.ipynb
+# Train Your Own Model and Convert It to TFLite
+
+# This notebook uses the [Fashion MNIST](https://github.com/zalandoresearch/fashion-mnist)
+# dataset which contains 70,000 grayscale images in 10 categories. The images
+# show individual articles of clothing at low resolution (28 by 28 pixels), as
+# seen here:
+# Fashion MNIST is intended as a drop-in replacement for the classic
+# [MNIST](http://yann.lecun.com/exdb/mnist/) dataset—often used as the "Hello,
+# World" of machine learning programs for computer vision. The MNIST dataset
+# contains images of handwritten digits (0, 1, 2, etc.) in a format identical to
+# that of the articles of clothing we'll use here.
+# This uses Fashion MNIST for variety, and because it's a slightly more
+# challenging problem than regular MNIST. Both datasets are relatively small and
+# are used to verify that an algorithm works as expected. They're good starting
+# points to test and debug code.
+# We will use 60,000 images to train the network and 10,000 images to evaluate
+# how accurately the network learned to classify images. You can access the
+# Fashion MNIST directly from TensorFlow. Import and load the Fashion MNIST data
+# directly from TensorFlow:
+
+# Setup
+
+import os
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import pathlib
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+print(tf.__version__)
+
+# Download Fashion MNIST Dataset
+# TODO: AttributeError: type object 'Split' has no attribute 'ALL'
+(train_examples, validation_examples, test_examples), info = tfds.load(
+    'fashion_mnist', with_info=True, as_supervised=True,
+    split=('train[:80%]', 'train[80%:]', 'test'))
+num_examples = info.splits['train'].num_examples
+num_classes = info.features['label'].num_classes
+
+class_names = ['T-shirt_top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+with open('fashiondata/labels.txt', 'w') as f:
+  f.write('\n'.join(class_names))
+
+IMG_SIZE = 28
+
+# Preprocessing data
+## Preprocess
+# Write a function to normalize and resize the images
+def format_example(image, label):
+  image = tf.cast(image, tf.float32)
+  image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
+  image = image / 255.0
+  return image, label
+
+# Set the batch size to 32
+BATCH_SIZE = 32
+
+## Create a Dataset from images and labels
+# Prepare the examples by preprocessing them and then batching them (and
+# optionally prefetching them)
+
+asis=tf.autograph.experimental.do_not_convert
+# If you wish you can shuffle train set here
+train_batches = train_examples.cache().shuffle(
+  num_examples//4).batch(BATCH_SIZE).map(asis(format_example)).prefetch(1)
+validation_batches = validation_examples.cache().batch(
+  BATCH_SIZE).map(asis(format_example)).prefetch(1)
+test_batches = test_examples.cache().batch(1).map(asis(format_example))
+
+# Building the model
+"""
+Model: "sequential"
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #   
+=================================================================
+conv2d (Conv2D)              (None, 26, 26, 16)        160       
+_________________________________________________________________
+max_pooling2d (MaxPooling2D) (None, 13, 13, 16)        0         
+_________________________________________________________________
+conv2d_1 (Conv2D)            (None, 11, 11, 32)        4640      
+_________________________________________________________________
+flatten (Flatten)            (None, 3872)              0         
+_________________________________________________________________
+dense (Dense)                (None, 64)                247872    
+_________________________________________________________________
+dense_1 (Dense)              (None, 10)                650       
+=================================================================
+Total params: 253,322
+Trainable params: 253,322
+Non-trainable params: 0
+"""
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Conv2D(16, 3, activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 1)),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(64, activation='relu'),
+  tf.keras.layers.Dense(10)
+])
+
+# Set the loss and accuracy metrics
+model.compile(
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer='adam',
+    metrics=['accuracy'])
+
+export_dir = 'fashiondata/1'
+if not os.path.exists(export_dir):
+  model.fit(train_batches, 
+            epochs=10,
+            validation_data=validation_batches,
+            verbose=0)
+  tf.saved_model.save(model, export_dir)
+
+mode = "Speed" #@param ["Default", "Storage", "Speed"]
+if mode == 'Storage':
+  optimization = tf.lite.Optimize.OPTIMIZE_FOR_SIZE
+elif mode == 'Speed':
+  optimization = tf.lite.Optimize.OPTIMIZE_FOR_LATENCY
+else:
+  optimization = tf.lite.Optimize.DEFAULT
+
+# Invoke the converter to finally generate the TFLite model
+converter = tf.lite.TFLiteConverter.from_saved_model(export_dir)
+converter.optimizations = [optimization]
+tflite_model = converter.convert()
+
+tflite_model_file = 'fashiondata/model.tflite'
+
+with open(tflite_model_file, "wb") as f:
+  f.write(tflite_model)
+
+# Test if your model is working
+
+# ==== problem with input format
+# https://stackoverflow.com/questions/50764572/how-can-i-test-a-tflite-model-to-prove-that-it-behaves-as-the-original-model-us
+# https://stackoverflow.com/questions/50443411/how-to-load-a-tflite-model-in-script/51986982#51986982
+
+# Load TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path=tflite_model_file)
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Test model on random input data.
+input_shape = input_details[0]['shape']
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+interpreter.invoke()
+
+# The function `get_tensor()` returns a copy of the tensor data.
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
+
+# ==== continue original tutorial
+
+input_index = interpreter.get_input_details()[0]["index"]
+output_index = interpreter.get_output_details()[0]["index"]
+
+# Gather results for the randomly sampled test images
+predictions = []
+test_labels = []
+test_images = []
+
+for img, label in test_batches.take(50):
+  #img, label = tuple(test_batches.take(1))[0]
+  interpreter.set_tensor(input_index, img)
+  interpreter.invoke()
+  predictions.append(interpreter.get_tensor(output_index))
+  test_labels.append(label[0])
+  test_images.append(np.array(img))
+
+#@title Utility functions for plotting
+# Utilities for plotting
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  img = np.squeeze(img)
+  plt.imshow(img, cmap=plt.cm.binary)
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label.numpy():
+    color = 'green'
+  else:
+    color = 'red'
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks(list(range(10)), class_names, rotation='vertical')
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array[0], color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array[0])
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('green')
+
+#@title Visualize the outputs { run: "auto" }
+index = 49 #@param {type:"slider", min:1, max:50, step:1}
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(index, predictions, test_labels, test_images)
+plt.show()
+plot_value_array(index, predictions, test_labels)
+plt.show()
+
+# # Download TFLite model and assets
+# 
+# **NOTE: You might have to run to the cell below twice**
+# try:
+#   from google.colab import files
+#   files.download(tflite_model_file)
+#   files.download('fashiondata/labels.txt')
+# except:
+#   pass
+# # Deploying TFLite model
+# 
+# # Now once you've the trained TFLite model downloaded, you can ahead and deploy
+# # this on an Android/iOS application by placing the model assets in the
+# # appropriate location.
+# 
+# # Prepare the test images for download (Optional)
+# 
+# # !mkdir -p test_images
+# 
+# from PIL import Image
+# 
+# for index, (image, label) in enumerate(test_batches.take(50)):
+#   image = tf.cast(image * 255.0, tf.uint8)
+#   image = tf.squeeze(image).numpy()
+#   pil_image = Image.fromarray(image)
+#   pil_image.save('test_images/{}_{}.jpg'.format(class_names[label[0]].lower(), index))
+# 
+# # !ls test_images
+# 
+# # !zip -qq fmnist_test_images.zip -r test_images/
+# 
+# try:
+#   files.download('fmnist_test_images.zip')
+# except:
+#   pass
